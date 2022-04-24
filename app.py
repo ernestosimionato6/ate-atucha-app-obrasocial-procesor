b63bcb73655144ec0afa6e13f83f493da47a6208
ernestosimionato6 <102230014+ernestosimionato6@users.noreply.github.com>
2022-04-24 07:31:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ if pd_notacredito_origin.empty == False:
 st.subheader("Los datos consolidados aparecerán debajo 👇")
 st.text("")
 
-st.dataframe(df_consolidado.style.format("{:.2%}"))
+st.dataframe(df_consolidado)
     
 st.text("")
 
